e9e0f023d483d3c3329f44e4bb413d7f0187c1a9
D1n8 <mr.dinyar@mail.ru>
2025-04-17 19:55:00 +0500

    add icons animation and update margins

--- portfolio/src/components/header.tsx
+++ portfolio/src/components/header.tsx
@@ -1,15 +1,12 @@
 export default function Header() {
     return (
-    <header className="header">
-         {/* <h1 className="header__title">Сайт портфолио</h1> */}
-         <nav>
-            <ul className="nav header__nav">
-                <li className="nav__item"><a href="#about">Обо мне</a></li>
-                <li className="nav__item"><a href="#projects">Проекты</a></li>
-                <li className="nav__item"><a href="#contacts">Контакты</a></li>
-            </ul>
-         </nav>
-    </header>
+        <header className="header">
+            <nav className="nav header__nav">
+                <a className="nav__item" href="#about">Обо мне</a>
+                <a className="nav__item" href="#projects">Проекты</a>
+                <a className="nav__item" href="#contacts">Контакты</a>
+            </nav>
+        </header>
     )
-    
+
 }
--- portfolio/src/components/projects.tsx
+++ portfolio/src/components/projects.tsx
@@ -8,10 +8,10 @@ export default function Projects() {
             <span>Проекты</span>
         </h2>
         <div className="projects__container">
-            <ProjectElem name='weather' src="../../public/img/weather_app_screen.png" descr="Веб приложение 'Погода' на VanillaJS. Приложение определяет страну, город, дату, время, температуру воздуха и скорость ветра по указанным данным (долготе и широте). Настроено взаимодействие c API карты. Есть возможность создавать несколько виджетов с независимыми друг от друга данными." />
-            <ProjectElem name='antools' src="../../public/img/antools_screen.png" descr="Flexbox верстка сайта Antools с использованием препроцессора SASS/SCSS по БЭМ методологии с анимациями." />
+            <ProjectElem name='Weather-App' src="../../public/img/weather_app_screen.png" descr="Веб приложение 'Погода' на VanillaJS. Приложение определяет страну, город, дату, время, температуру воздуха и скорость ветра по указанным данным (долготе и широте). Настроено взаимодействие c API карты. Есть возможность создавать несколько виджетов с независимыми друг от друга данными." />
+            <ProjectElem name='antools_layout' src="../../public/img/antools_screen.png" descr="Flexbox верстка сайта Antools с использованием препроцессора SASS/SCSS по БЭМ методологии с анимациями." />
             {/* <ProjectElem name='euclid' src="../../public/img/euclid_site_screen.png" descr="описание" /> */}
-            <ProjectElem name='roomtibet' src="../../public/img/roomTibet_screen.png" descr="Адаптивная flexbox верстка сайта RoomTibet с анимациями." />
+            <ProjectElem name='layout_RoomTibet' src="../../public/img/roomTibet_screen.png" descr="Адаптивная flexbox верстка сайта RoomTibet с анимациями." />
         </div>
     </section>
     )
